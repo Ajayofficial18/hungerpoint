84091cf6493777f6af55f63ae1ef69299fc58060
Ajay <ajgour14@gmail.com>
2024-06-22 08:17:27 +0530

    added backend

--- App/src/Components/Footer/Footer.jsx
+++ App/src/Components/Footer/Footer.jsx
@@ -5,32 +5,32 @@ const Footer = () => {
         <footer className={Style.foot}>
             <img src="./images/logo.png" alt="logo"></img>
             <div className={Style.elements}>
-                <div className={Style.Product1}>
+                <div className={Style.ulcont}>
                     <span>Product</span>
-                    <ul className={Style.Product}>
+                    <ul className={Style.ulcls}>
                         <li>Feature</li>
                         <li>Integrations</li>
                         <li>Pricing</li>
                         <li>FAQ</li>
                     </ul>
                 </div>
-                <div className={Style.Company1}>
+                <div className={Style.ulcont}>
                     <span>Company</span>
-                    <ul className={Style.Company}>
+                    <ul className={Style.ulcls}>
                         <li>Privacy</li>
                         <li>Terms and Services</li>
                     </ul>
                 </div>
-                <div className={Style.Developers1}>
+                <div className={Style.ulcont}>
                     <span>Developers</span>
-                    <ul className={Style.Developers}>
+                    <ul className={Style.ulcls}>
                         <li>Documentation</li>
                         <li>Guide</li>
                     </ul>
                 </div>
-                <div>
+                <div className={Style.ulcont}>
                     <span>Social media</span>
-                    <ul className={Style.Social}>
+                    <ul className={Style.ulcls}>
                         <li>facebook</li>
                         <li>twitter</li>
                         <li>instagram</li>
